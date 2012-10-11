fde3b8874dc703998b5598ae6f20c73ae03fac40
Steven Laan <therealneojume@gmail.com>
2012-10-11 12:59:45 +0200

    logsumexp comment

--- src/lab06/logsumexp.m
+++ src/lab06/logsumexp.m
@@ -1,3 +1,9 @@
+% function Log = logsumexp( LN1, LN2 )
+% 
+% Computes the sum of the two given log probabilities in an efficient way.
 function Log = logsumexp( LN1, LN2 )
-Log = LN1 + log( ones( size(LN1) ) + exp( sign( LN2 - LN1) * abs( LN2 - LN1 ) ) );
+    % The sign function flips the sign of the absolute value, if the other
+    % log was greater. This ensures we take the right value (and is faster
+    % than an if-statement).
+    Log = LN1 + log(ones(size(LN1)) + exp(sign(LN2 - LN1) * abs(LN2 - LN1)));
 end
