6e4086521277e0d2ae82015fac3a6b1068c67aa5
Steven Laan <therealneojume@gmail.com>
2012-10-11 13:12:16 +0200

    puntjes op de keer

--- src/lab06/logsumexp.m
+++ src/lab06/logsumexp.m
@@ -5,5 +5,5 @@ function Log = logsumexp( LN1, LN2 )
     % The sign function flips the sign of the absolute value, if the other
     % log was greater. This ensures we take the right value (and is faster
     % than an if-statement).
-    Log = LN1 + log(ones(size(LN1)) + exp(sign(LN2 - LN1) * abs(LN2 - LN1)));
+    Log = LN1 + log(ones(size(LN1)) + exp(sign(LN2 - LN1) .* abs(LN2 - LN1)));
 end
