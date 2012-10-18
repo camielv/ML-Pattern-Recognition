275958aaecf6c0acda83fe281046f74f447b23dd
Steven Laan <therealneojume@gmail.com>
2012-10-18 14:22:16 +0200

    awesome plots

--- src/lab4/GP.m
+++ src/lab4/GP.m
@@ -4,7 +4,28 @@ sigma = [1,0;0,3];
 
 [X, Y] = meshgrid( linspace(-5, 5), linspace(-5, 5) );
 data = [X(:), Y(:)];
+Z = mvnpdf( data, mu, sigma );
+subplot(3,1,1)
+contour( X, Y, reshape(Z, size(X,1), size(Y,1) ) )
+xlabel( '$x_1$', 'interpreter', 'latex' );
+ylabel( '$x_2$', 'interpreter', 'latex' );
 
+subplot(3,1,2)
+data2 = mvnpdf( X(1,:)', mu(1), sigma(1,1) );
+plot( data2 );
+xlabel( '$x_1$', 'interpreter', 'latex' );
+ylabel( '$P(x_1)$', 'interpreter', 'latex' );
 
+subplot( 3,1,3 )
+x2 = -3: 1 : 3;
+cc = hsv(size(x2,2));
 
-%%
+hold all
+for i = 1:size(x2,2)
+    data3 = mvnpdf( [ X(1,:)', repmat( x2(i), size(X,1), 1 )  ], mu, sigma );
+    plot( data3 ); %, 'color', cc(i,:) )
+end
+
+xlabel( '$x_1$', 'interpreter', 'latex' );
+ylabel( '$P(x_1|x_2)$', 'interpreter', 'latex' );
+hold off
