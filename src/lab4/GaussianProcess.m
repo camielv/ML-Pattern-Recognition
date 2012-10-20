c6f5889a5a8679c48ede0e8ba4777bf5ee695475
Steven Laan <therealneojume@gmail.com>
2012-10-20 13:00:12 +0200

    meer for loops

--- src/lab4/GaussianProcess.m
+++ src/lab4/GaussianProcess.m
@@ -5,6 +5,12 @@ function [ f_star, noise_star, LL ] = GaussianProcess( X, t, noise, x_star, K, k
     v = L\k_star;
     noise_star = covariance_function( x_star, x_star, theta, l ) - v'* v;
     n = size(X, 1);
-    LL = -0.5 * t' * alpha - trace( log( L ) ) - n / 2 * log( 2 * pi ); 
+    
+    sum = 0;
+    for i = 1:n
+        sum = sum + log(L(i,i)) - ( n/2 * log( 2 * pi ) );
+    end
+    LL = -0.5 * t' * alpha - sum;
+    %LL = -0.5 * t' * alpha - trace( log( L ) ) - ( n / 2 * log( 2 * pi ) ); 
 end
 
